ff16d099a4af69d2b20a14921c4d89b060a80cec
dyarfi <defrian.yarfi@gmail.com>
2017-04-18 15:36:58 +0700

    Css and Views

--- application/config/config.php
+++ application/config/config.php
@@ -244,7 +244,7 @@ $config['encryption_key'] = 'encryption+**&&^^%%$RGGGFR$&&';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']	= 'session_'.md5('si');
+$config['sess_cookie_name']	= 'session_si';
 $config['sess_expiration']	= 0;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
